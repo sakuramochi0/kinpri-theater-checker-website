6e0644761a24cc119f84f7ab93e22dd2b0b955ff
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-06-14 01:06:02 +0900

    refactoring view code

--- views.py
+++ views.py
@@ -19,17 +19,24 @@ def make_shows(db, theater, day):
 
 def index(request):
     db = get_mongo_client().kinpri_theater_checker
+    last_updated = db.theaters.find().sort(
+        [('last_updated', -1)]).limit(1)[0]['last_updated']
     today = datetime.datetime.fromordinal(datetime.date.today().toordinal())
     day_num = 4
     days = [today + datetime.timedelta(days=i) for i in range(day_num)]
+    theater_shows = [(theater,
+                      [make_shows(db, theater['name'], day) for day in days])
+                     for theater in db.theaters.find()]
+    support_theater_num = len(db.shows_latest.distinct('theater'))
+    present_total_theater_num = db.theaters.find({'start_date': parse('6/10')}).count()
+    total_theater_num = db.theaters.find().count()
     return render(request, 'kinpri_theater_checker/index.html', {
-        'last_updated': db.theaters.find().sort(
-            [('last_updated', -1)]).limit(1)[0]['last_updated'],
+        'last_updated': last_updated,
         'days': days,
-        'theater_shows': [(
-            theater,
-            [make_shows(db, theater['name'], day) for day in days]
-        ) for theater in db.theaters.find()],
+        'theater_shows': theater_shows,
+        'support_theater_num': support_theater_num,
+        'total_theater_num': total_theater_num,
+        'present_total_theater_num': present_total_theater_num,
     })
 
 def theater(request):
